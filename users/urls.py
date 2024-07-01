8dd3abd2a3bd8ac86fcc2f503096cdaf2276bfdf
Folafolu <ofolafolu@gmail.com>
2024-07-01 13:29:29 +0100

    user register

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import Register
+
+
+urlpatterns = [
+    path("signup", Register.as_view(), name="signup")
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
+from rest_framework import generics
+from .serializers import RegisterSerializer
 # Create your views here.
+
+
+class Register(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
