8a2b3065254b29c850abfeb430c0bf391f57d2a9
oghenekevbe <oghenekevbe.egume@gmail.com>
2024-07-08 03:30:26 +0100

    reset password by email link done + tests. ..

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, PasswordResetView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +9,5 @@ urlpatterns = [
     path("signup/", RegisterView.as_view(), name="signup"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/password/reset/', PasswordResetView.as_view(), name='password_reset'),
 ]
