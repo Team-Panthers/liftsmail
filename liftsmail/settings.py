7916999fde9825b7b71e16f58922b9a8502a09bb
Folafolu <ofolafolu@gmail.com>
2024-07-16 19:24:40 +0100

    fix

--- liftsmail/settings.py
+++ liftsmail/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -101,13 +101,12 @@ WSGI_APPLICATION = 'liftsmail.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASS'),
-        "HOST": os.getenv('DB_HOST'),
+        "NAME": "nzksrilc",
+        "USER": "nzksrilc",
+        "PASSWORD": "TzaqUbD8aDjDgacJBQlAx8q-l8iio11M",
+        "HOST": "bubble.db.elephantsql.com",
     }
 }
-
 
 
 
